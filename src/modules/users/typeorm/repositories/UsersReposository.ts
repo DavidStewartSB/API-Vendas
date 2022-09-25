cac3be2f6c27d01b4eeaa19960718fd2a848d6ef
David Stewart <david.stewartsb@gmail.com>
2022-09-25 18:12:37 -0300

    add: UserRepository and UserEntity

---
+++ src/modules/users/typeorm/repositories/UsersReposository.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { EntityRepository, Repository } from "typeorm"
+import User from "../entities/User"
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+    public async findByName(name: string): Promise<User | any> {
+        const user = await this.findOne({ where: name })
+        return user
+    }
+
+    public async findById(id: string): Promise<User | any> {
+        const user = await this.findOne({ where: id })
+        return user
+    }
+
+    public async findByEmail(email: string): Promise<User | any> {
+        const user = await this.findOne({ where: email })
+        return user
+    }
+}
