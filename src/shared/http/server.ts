215c6672558f9151600b62604b56942c11ccccf2
David Stewart <david.stewartsb@gmail.com>
2022-09-26 22:43:18 -0300

    fix: response errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata"
 import express, { NextFunction, Request, Response } from "express"
+import "express-async-errors"
 import cors from "cors"
 import routes from "./routes"
-import "express-async-errors"
-import AppError from "@shared/errors/AppError"
 import "@shared/typeorm"
 import { errors } from "celebrate"
+import { AppError } from "@shared/errors/AppError"
 
 const app = express()
 
@@ -14,12 +14,12 @@ app.use(express.json())
 
 app.use(routes)
 app.use(errors())
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof AppError)
         return res
-            .status(err.statusCode)
-            .json({ status: "error", message: err.message })
-    }
+            .status(error.statusCode)
+            .json({ status: "error", message: error.message })
 
     return res
         .status(500)
